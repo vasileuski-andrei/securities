4e0558eca61d0b9c3863f373a26113f8e7fd5643
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-07-18 21:49:40 +0300

    add crypto-exmo module

---
+++ crypto-exmo/src/main/java/com/starlight/cryptoexmo/CryptoExmoApplication.java
@@ -0,0 +1,13 @@
+package com.starlight.cryptoexmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoExmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoExmoApplication.class, args);
+    }
+
+}
